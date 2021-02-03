4c2503efd93849a913ef1c361e0d2c457f8a38f7
Tamas Szabados <tszabad@gmail.com>
2021-02-03 11:24:52 +0100

    salt added to the app

--- password_cracker.py
+++ password_cracker.py
@@ -3,21 +3,32 @@ import hashlib
 def crack_sha1_hash(hash, use_salts=False):
   
   with open('top-10000-passwords.txt', 'r') as f:
+    passwords = []
     f = f.read().splitlines()
     if use_salts==True:
-      pass
-    
+      
+      with open("known-salts.txt", 'r') as ks:
+        ks = ks.read().splitlines()
+        for line in f:
+          for salt in ks:
+            new_hash1 = hashlib.sha1((line+salt).encode()).hexdigest()
+            new_hash2 = hashlib.sha1((salt+line).encode()).hexdigest()
+            new_hash3 = hashlib.sha1((salt+line+salt).encode()).hexdigest()
+            if new_hash1 == hash or new_hash2 == hash or              new_hash3 == hash:
+              print(line)
+              return line
 	  
     else:
-      passwords = []
       for line in f:
         new_hash = hashlib.sha1(line.encode()).hexdigest()
         if new_hash== hash:
           passwords.append(line)
           print(line)
           return line
-      if len(passwords)<1:
-        return "PASSWORD NOT IN DATABASE"
+          
+    if len(passwords)<1:
+      return "PASSWORD NOT IN DATABASE"
+      
       
       
         
